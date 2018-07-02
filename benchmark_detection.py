64d7d9b8add1f85484f7e7c6df1205af243ef272
ridouaneg <ridouaneg@gmail.com>
2018-07-02 15:44:38 +0200

    Création de utils.py avec les fonctions read_image (path of an image -> numpy array) et read_label (path of a label -> list)

--- benchmark_detection.py
+++ benchmark_detection.py
@@ -1,1 +1,8 @@
 # Compare detection methods thanks to metrics and a dev set
+import utils
+
+path_image = "images/fifa_test_frame100.jpg"
+img = read_image(path_image)
+
+path_label = "labels/fifa_test_frame100.xml"
+bounding_boxes = read_label(path_label)
