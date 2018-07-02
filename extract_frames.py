cb457fe8c5cefe620609d2aebaaf8289e03cb92b
ridouaneg <ridouaneg@gmail.com>
2018-07-02 09:56:45 +0200

    Création du fichier python (vide) extract_frame.py

---
+++ extract_frames.py
@@ -0,0 +1,7 @@
+# Extraire les frames des vidéos présentes dans le dossier ressources
+# et les enregistrer au format .jpg dans un sous-dossier de ressources
+# dont le nom est nom_de_la_video_frames, les frames ayant les noms suivants
+# nom_de_la_video_frame1, nom_de_la_video_frame2, ...
+
+# Après labellisation, enregistrer les labels au chemin DeepFootballAnalysis/
+# ressources/nom_de_la_video_labels/
